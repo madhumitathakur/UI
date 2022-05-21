57fb70c75214e0927b34ea1f295edb2fbdcd0c13
raja <raja.m@zensar.com>
2022-05-22 01:18:29 +0530

    interview table

--- src/app/components/panel-form/panel-form.component.ts
+++ src/app/components/panel-form/panel-form.component.ts
@@ -8,16 +8,16 @@ import { PannelService } from 'src/app/services/pannel.service';
   templateUrl: './panel-form.component.html',
   styleUrls: ['./panel-form.component.css']
 })
-export class PanelFormComponent  {
+export class PanelFormComponent {
 
   forms: any = [];
   pannelMembersDetails: Array<Pannel> = new Array();
 
 
-  
-  constructor(private  pannelService:  PannelService, private formBuilder: FormBuilder) {
 
-    this. pannelService.getAllPannelMembers().subscribe((serverResponse: any) => {
+  constructor(private pannelService: PannelService, private formBuilder: FormBuilder) {
+
+    this.pannelService.getAllPannelMembers().subscribe((serverResponse: any) => {
       console.log('constrcutor serverResponse ', serverResponse);
       this.forms = serverResponse;
     })
@@ -33,24 +33,22 @@ export class PanelFormComponent  {
   })
 
   submitPannelForm = () => {
-
+    console.log("locatiomncheck ", this.pannelForm.value);
     var pannel_form = {
       employeeId: this.pannelForm.value['employeeId'],
       name: this.pannelForm.value['name'],
       type: this.pannelForm.value['type'],
-
       location: this.pannelForm.value['location']
-      // date: this.interviewForm.value['date']
     };
 
-  this.pannelService.createNewPannel(pannel_form).subscribe((serverResponse: any) => {
-    console.log('createNewPannel - serviceResponse : ', serverResponse);
+    this.pannelService.createNewPannel(pannel_form).subscribe((serverResponse: any) => {
+      console.log('createNewPannel - serviceResponse : ', serverResponse);
 
-    this.forms.push(serverResponse);
-  })
+      this.forms.push(serverResponse);
+    })
 
-  console.log(this.pannelForm.value);
-  this.pannelService.addPannels(pannel_form);
+    console.log(this.pannelForm.value);
+    this.pannelService.addPannels(pannel_form);
 
-}
+  }
 }
