37b020725f3da105bbb297c6b165ffe2dd02284a
Satyam Rakhonde <satyam.rakhonde@zensar.com>
2022-05-22 18:19:19 +0530

    Candidate service compiled successfully

--- src/app/components/candidate-form/candidate-form.component.ts
+++ src/app/components/candidate-form/candidate-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Candidate } from 'src/app/entities/candidate.entity';
+import { CandidateService } from 'src/app/services/candidate.service';
 
 @Component({
   selector: 'app-candidate-form',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandidateFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private candidateService : CandidateService) { 
+    this.candidateService.getAllCandidate().subscribe((serverResponse: any) => {
+
+      console.log('constrcutor serverResponse ', serverResponse);
+      this.candidates = serverResponse;
+
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  candidates: any = [];
+  candidateDetails: Array<Candidate> = new Array();
+
+  candidateForm = new FormGroup({
+    
+    candidateName: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    primarySkills: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    secondarySkills: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    experience: new FormControl(null, [Validators.required]),
+    qualification: new FormControl(null, [Validators.required]),
+    designation: new FormControl(null, [Validators.required]),
+    noticePeriod: new FormControl(null, [Validators.required]),
+    location: new FormControl(null, [Validators.required])
+  })
+
+  submitCandidateForm = () => {
+
+    var candidate_form = {
+      // candidateId:
+      candidateName: this.candidateForm.value['candidateName'],
+      primarySkills: this.candidateForm.value['primarySkills'],
+      secondarySkills: this.candidateForm.value['secondarySkills'],
+      experience: this.candidateForm.value['experience'],
+      qualification: this.candidateForm.value['qualification'],
+      designation: this.candidateForm.value['designation'],
+      noticePeriod: this.candidateForm.value['noticePeriod'],
+      location: this.candidateForm.value['location']
+    };
+
+    this.candidateService.createNewInterview(candidate_form).subscribe((serverResponse: any) => {
+      console.log('createNewCandidate - serviceResponse : ', serverResponse);
+
+      this.candidates.push(serverResponse);
+    })
+
+    console.log(this.candidateForm.value);
+}
 }
---
+++ src/app/entities/candidate.entity.ts
@@ -0,0 +1,13 @@
+export class Candidate{
+
+    candidateId:number = 0;
+    candidateName:String = '';
+    primarySkills:String = '';
+    secondarySkills:String = '';
+    experience:number = 0;
+    qualification:String = '';
+    designation:String = '';
+    noticePeriod:number = 0;
+    location:String = '';
+
+}
